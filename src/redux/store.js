e4e6818a2aff1880367a58acc0c186d27922c80c
Azamat Toxanbayev <h7559006@gmail.com>
2024-07-25 20:08:00 +0500

    Second commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { PostReducer } from "./slices/posts";
+import { authReducer } from "./slices/auth";
 
 const store = configureStore({
   reducer: {
     posts: PostReducer,
+    auth: authReducer,
   },
 });
 
